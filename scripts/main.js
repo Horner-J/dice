7032820ed2e599103767de914dc5812f857b99ff
Horner-J <jasonandrewhorner@gmail.com>
2022-05-22 10:57:28 -0600

    Adding button fucitonality
    Adding "roll" button to perfomr fuction diceRoll

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
 const resultDisplay = document.getElementById('result');
-const rollDiceButton = document.getElementById('roll)');
 
-let diceResult 
-
-rollDiceButton.addEventListener('click', event => {
-    diceRoll();
-});
+document.getElementById('roll').addEventListener('click', diceRoll);
 
 function diceRoll() {
     const diceResult = Math.floor(Math.random() * 6) + 1;
-    
     console.log(diceResult);
-
     resultDisplay.innerHTML = diceResult;
 }
