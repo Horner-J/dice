154014ac87ce33c15dbf6bd6a220e85ad451e6b0
Horner-J <jasonandrewhorner@gmail.com>
2022-05-22 12:54:25 -0600

    Changing dice pictures

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,22 @@ function diceRoll() {
     let diceResult = Math.floor(Math.random() * 6) + 1;
 
     if (diceResult === 1) {
-        resultDisplay.innerHTML = "<img src='../images/dice-1.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-1.jpg'>";
     } 
     if (diceResult === 2) {
-        resultDisplay.innerHTML = "<img src='../images/dice-2.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-2.jpg'>";
     } 
     if (diceResult === 3) {
-        resultDisplay.innerHTML = "<img src='../images/dice-3.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-3.jpg'>";
     } 
     if (diceResult === 4) {
-        resultDisplay.innerHTML = "<img src='../images/dice-4.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-4.jpg'>";
     } 
     if (diceResult === 5) {
-        resultDisplay.innerHTML = "<img src='../images/dice-5.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-5.jpg'>";
     } 
     if (diceResult === 6) {
-        resultDisplay.innerHTML = "<img src='../images/dice-6.png'>";
+        resultDisplay.innerHTML = "<img src='../images/dice-6.jpg'>";
     } 
 
     console.log(diceResult);
