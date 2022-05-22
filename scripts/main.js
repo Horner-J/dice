b17153a083e52d4ac37f7e0abb6690fab2925dc2
Horner-J <jasonandrewhorner@gmail.com>
2022-05-22 13:10:09 -0600

    Moving images to try to fix image link issues.

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,22 @@ function diceRoll() {
     let diceResult = Math.floor(Math.random() * 6) + 1;
 
     if (diceResult === 1) {
-        resultDisplay.innerHTML = "<img src='../images/dice-1.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-1.jpg'>";
     } 
     if (diceResult === 2) {
-        resultDisplay.innerHTML = "<img src='../images/dice-2.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-2.jpg'>";
     } 
     if (diceResult === 3) {
-        resultDisplay.innerHTML = "<img src='../images/dice-3.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-3.jpg'>";
     } 
     if (diceResult === 4) {
-        resultDisplay.innerHTML = "<img src='../images/dice-4.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-4.jpg'>";
     } 
     if (diceResult === 5) {
-        resultDisplay.innerHTML = "<img src='../images/dice-5.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-5.jpg'>";
     } 
     if (diceResult === 6) {
-        resultDisplay.innerHTML = "<img src='../images/dice-6.jpg'>";
+        resultDisplay.innerHTML = "<img src='scripts/images/dice-6.jpg'>";
     } 
 
     console.log(diceResult);
