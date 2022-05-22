0c7fa70664550552497b072aa7ec5208b0eb2758
Horner-J <jasonandrewhorner@gmail.com>
2022-05-22 12:39:05 -0600

    Adding dice icon instead of text

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,27 @@ const resultDisplay = document.getElementById('result');
 document.getElementById('roll').addEventListener('click', diceRoll);
 
 function diceRoll() {
-    const diceResult = Math.floor(Math.random() * 6) + 1;
+    let diceResult = Math.floor(Math.random() * 6) + 1;
+
+    if (diceResult === 1) {
+        resultDisplay.innerHTML = "<img src='../images/dice-1.png'>";
+    } 
+    if (diceResult === 2) {
+        resultDisplay.innerHTML = "<img src='../images/dice-2.png'>";
+    } 
+    if (diceResult === 3) {
+        resultDisplay.innerHTML = "<img src='../images/dice-3.png'>";
+    } 
+    if (diceResult === 4) {
+        resultDisplay.innerHTML = "<img src='../images/dice-4.png'>";
+    } 
+    if (diceResult === 5) {
+        resultDisplay.innerHTML = "<img src='../images/dice-5.png'>";
+    } 
+    if (diceResult === 6) {
+        resultDisplay.innerHTML = "<img src='../images/dice-6.png'>";
+    } 
+
     console.log(diceResult);
-    resultDisplay.innerHTML = diceResult;
+    // resultDisplay.innerHTML = diceResult;
 }
